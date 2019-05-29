2d0e4075a88098b04a92f064b8aada067c04d46d
zigzi salah eddine <s.zigzi@gmail.com>
2019-05-29 05:14:34 +0000

    enable cross origin

--- src/main/java/com/zigdev/app/repositories/PlayerRepository.java
+++ src/main/java/com/zigdev/app/repositories/PlayerRepository.java
@@ -3,7 +3,9 @@ package com.zigdev.app.repositories;
 import com.zigdev.app.entitites.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RepositoryRestResource
 public interface PlayerRepository extends JpaRepository<Player,Long> {
 
--- src/main/java/com/zigdev/app/repositories/TeamRepository.java
+++ src/main/java/com/zigdev/app/repositories/TeamRepository.java
@@ -3,7 +3,9 @@ package com.zigdev.app.repositories;
 import com.zigdev.app.entitites.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RepositoryRestResource
 public interface TeamRepository extends JpaRepository<Team,Long> {
 }
